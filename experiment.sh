053e118e09e1b6f08b9876df8940423f7cd8a2c0
David Noursi <dnoursi@uchicago.edu>
2019-02-11 14:07:58 -0600

    Better experiments

--- experiment.sh
+++ experiment.sh
@@ -1,7 +1,7 @@
 
 
-export OPENAI_LOGDIR="/Users/dcalifornia/baselines/experiment_logs/experiment3"
-python3 -m baselines.run --alg=deepq --env=graph-search-v0 --num_timesteps=1e4
+export OPENAI_LOGDIR="/Users/dcalifornia/baselines/experiment_logs/experiment7"
+python3 -m baselines.run --alg=deepq --env=graph-search-v0 --num_timesteps=2e3 --print_freq=10
 
 
 
